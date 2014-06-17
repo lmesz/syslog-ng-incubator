c1fe45fb27f02dbc02b09e6f7ce40bf01ec2683c
Laszlo Meszaros <lmesz@balabit.hu>
2014-06-17 08:43:51 +0200

    ZMQ: source basics yet
     * proto-transport ...
     * Need to implement handled_in function: zmq_sd_accept based on afsocket

--- modules/zmq/zmq-source.c
+++ modules/zmq/zmq-source.c
@@ -33,8 +33,69 @@
 #include "driver.h"
 #include "plugin-types.h"
 
-void
+static void
+zmq_sd_start_watches(ZMQSourceDriver *self)
+{
+  IV_FD_INIT(&self->listen_fd);
+  self->listen_fd.fd = self->fd;
+  self->listen_fd.cookie = self;
+  self->listen_fd.handler_in = NULL; /*TODO: Handle data receiving!!*/
+  iv_fd_register(&self->listen_fd);
+}
+
+static gboolean
+zmq_sd_init(LogPipe *s)
+{
+  ZMQSourceDriver *self = (ZMQSourceDriver *) s;
+
+  GlobalConfig *cfg = log_pipe_get_config(s);
+
+  self->proto_factory = log_proto_server_get_factory(cfg, "zmq");
+
+  log_reader_options_init(&self->reader_options, cfg, self->super.super.group);
+
+  zmq_sd_start_watches(self);
+
+  return log_src_driver_init_method(s);
+}
+
+static void
+zmq_sd_queue(LogPipe *s, LogMessage *msg, const LogPathOptions *path_options, gpointer user_data)
+{
+  log_pipe_forward_msg(s, msg, path_options);
+}
+
+static gboolean
+zmq_sd_deinit(LogPipe *s)
+{
+  ZMQSourceDriver *self = (ZMQSourceDriver *) s;
+  return TRUE;
+}
+
+static void
+zmq_sd_notify(LogPipe *s)
+{
+  ZMQSourceDriver *self = (ZMQSourceDriver *) s;
+}
+
+static void
+zmq_sd_free(LogPipe *s)
+{
+  ZMQSourceDriver *self = (ZMQSourceDriver *) s;
+}
+
+LogDriver *
 zmq_sd_new(GlobalConfig *cfg)
 {
-    msg_verbose("=================> ZMQ Source initialized!!!!!!!!1", evt_tag_str("driver", cfg->filename), NULL);
+  ZMQSourceDriver *self = g_new0(ZMQSourceDriver, 1);
+  log_src_driver_init_instance(&self->super);
+  self->super.super.super.init = zmq_sd_init;
+  self->super.super.super.queue = zmq_sd_queue;
+  self->super.super.super.deinit = zmq_sd_deinit;
+  self->super.super.super.notify = zmq_sd_notify;
+  self->super.super.super.free_fn = zmq_sd_free;
+  log_reader_options_defaults(&self->reader_options);
+
+  self->reader_options.parse_options.flags |= LP_LOCAL;
+  return &self->super.super;
 }
