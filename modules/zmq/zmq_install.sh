03087c32b8fdfb2887c2ba01c8eac30885659d63
Laszlo Meszaros <lmesz@balabit.hu>
2014-07-27 12:33:12 +0200

    travis-CI: work and execute tests
    Conflicts:
    
    	.travis.yml

--- modules/zmq/zmq_install.sh
+++ modules/zmq/zmq_install.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 wget https://github.com/zeromq/libzmq/archive/master.zip -O /tmp/master.zip
+
 cd /tmp
 unzip master.zip
-cd libzmq-master && ./autogen.sh && ./configure --prefix=/usr && make && make install
+mkdir $HOME/install/libzmq
+cd libzmq-master && ./autogen.sh && ./configure --prefix=$HOME/install/libzmq && make && make install
