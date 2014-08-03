56a83db0affff6fc9dbccccbb50329475585bae3
Laszlo Meszaros <lmesz@balabit.hu>
2014-08-03 10:21:36 +0200

    ZMQ: added 2 testcases that tests default working
     * It has to be executed manually
     * removed tests from travis later I will put it back

--- modules/zmq/scripts/zmq_test.sh
+++ modules/zmq/scripts/zmq_test.sh
@@ -1,20 +1,82 @@
 #!/bin/bash
 
-rm -f /tmp/zmq
+cleanup()
+{
+    rm -f /tmp/zmq
+}
 
-export PKG_CONFIG_PATH=$HOME/install/libzmq/lib/pkgconfig
-gcc pusher.c -o push -lzmq
+compile_pusher()
+{
+    gcc pusher.c -o push -lzmq
+}
 
-cp ./syslog-ng.conf ~/install/syslog-ng/etc/syslog-ng.conf
-~/install/syslog-ng/sbin/syslog-ng -f ./syslog-ng.conf
-./push 44444 1 &
-sleep 5
-num_of_lines=`grep -c Polip zmq`
+find_syslog_ng_pid()
+{
+    echo `ps aux | grep sbin/syslog-ng | grep -v grep | awk {'print $2'}`
+}
 
-if [ $num_of_lines == "1" ]
-then
-    kill -9 `ps aux | grep sbin/syslog-ng | grep -v grep | awk {'print $2'}`
-    exit 0
-fi
-kill -9 `ps aux | grep sbin/syslog-ng | grep -v grep | awk {'print $2'}`
-exit 1
+kill_syslog_ng()
+{
+    kill -9 $(find_syslog_ng_pid)
+}
+
+reload_syslog_ng()
+{
+    kill -1 $(find_syslog_ng_pid)
+}
+
+check_if_zmq_source_can_receive_one_message()
+{
+    echo "Starting testcase: check_if_zmq_source_can_receive_one_message"
+    cleanup
+    compile_pusher
+
+    cp ./syslog-ng.conf_port44444 ~/install/syslog-ng/etc/syslog-ng.conf
+    ~/install/syslog-ng/sbin/syslog-ng
+
+    ./push 44444 1 &
+    sleep 3
+    num_of_lines=`grep -c Polip /tmp/zmq`
+
+    if [ $num_of_lines == "1" ]
+    then
+            kill_syslog_ng
+            return 0
+    fi
+    kill_syslog_ng
+    echo "Failed testcase: check_if_zmq_source_can_receive_one_message"
+    exit 1
+}
+
+check_if_zmq_source_receive_messages_on_another_port_that_changed_during_reload()
+{
+    echo "Starting testcase: check_if_zmq_source_receive_messages_on_another_port_that_changed_during_reload"
+    cleanup
+    compile_pusher
+
+    cp ./syslog-ng.conf_port44444 ~/install/syslog-ng/etc/syslog-ng.conf
+    ~/install/syslog-ng/sbin/syslog-ng
+    ./push 44444 1 &
+
+    cp ./syslog-ng.conf_port44445 ~/install/syslog-ng/etc/syslog-ng.conf
+
+    reload_syslog_ng
+    ./push 44445 1 &
+
+    sleep 3
+    num_of_lines=`grep -c Polip /tmp/zmq`
+
+    if [ $num_of_lines == "2" ]
+    then
+            kill_syslog_ng 9
+            return 0
+    fi
+    kill_syslog_ng 9
+    echo "Failed testcase: check_if_zmq_source_can_receive_one_message"
+    exit 1
+
+}
+
+check_if_zmq_source_can_receive_one_message
+check_if_zmq_source_receive_messages_on_another_port_that_changed_during_reload
+echo "#######All testcases passed!#######"
