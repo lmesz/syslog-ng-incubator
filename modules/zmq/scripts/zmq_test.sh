a2e10713f8231db2888fc22b52f62df21ede2efc
Laszlo Meszaros <lmesz@balabit.hu>
2014-08-03 09:13:19 +0200

    ZMQ: travis and test fix

--- modules/zmq/scripts/zmq_test.sh
+++ modules/zmq/scripts/zmq_test.sh
@@ -2,11 +2,12 @@
 
 rm -f /tmp/zmq
 
+gcc pusher.c -o push -lzmq
 cp ./syslog-ng.conf ~/install/syslog-ng/etc/syslog-ng.conf
 ~/install/syslog-ng/sbin/syslog-ng
 ./push 44444 1 &
 sleep 5
-num_of_lines=`grep -c Polip /tmp/zmq`
+num_of_lines=`grep -c Polip zmq`
 
 if [ $num_of_lines == "1" ]
 then
