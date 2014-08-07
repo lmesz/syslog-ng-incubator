87539e9ca2ccd4d4668aee1a7168f61603f0a20e
Laszlo Meszaros <lmesz@balabit.hu>
2014-08-07 22:48:11 +0200

    ZMQ: trtrtrtr

--- modules/zmq/zmq_install.sh
+++ modules/zmq/zmq_install.sh
@@ -4,5 +4,4 @@ wget https://github.com/zeromq/libzmq/archive/master.zip -O /tmp/master.zip
 
 cd /tmp
 unzip master.zip
-mkdir -p $HOME/install/libzmq
-cd libzmq-master && ./autogen.sh && ./configure --prefix=$HOME/install/libzmq && make && make install
+cd libzmq-master && ./autogen.sh && ./configure && make && make install
