766d9a425a114c4ffc867df04801f85d62aaf123
Laszlo Meszaros <lmesz@balabit.hu>
2014-08-07 20:32:42 +0200

    ZMQ: travvv

--- modules/zmq/zmq_install.sh
+++ modules/zmq/zmq_install.sh
@@ -4,5 +4,5 @@ wget https://github.com/zeromq/libzmq/archive/master.zip -O /tmp/master.zip
 
 cd /tmp
 unzip master.zip
-mkdir $HOME/install/libzmq
+mkdir -p $HOME/install/libzmq
 cd libzmq-master && ./autogen.sh && ./configure --prefix=$HOME/install/libzmq && make && make install
