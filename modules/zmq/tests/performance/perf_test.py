986df6a8a8169357e4bcbb5171238d58f2432d33
Meszaros Laszlo <lacienator@gmail.com>
2016-04-25 08:05:52 +0200

    zmq: added final perf test it is a bit exaggeration

--- modules/zmq/tests/performance/perf_test.py
+++ modules/zmq/tests/performance/perf_test.py
@@ -6,6 +6,7 @@ import zmq
 import timeit
 import time
 import socket
+import SocketServer
 import threading
 
 conf_source = """
@@ -55,25 +56,43 @@ NUMBER_OF_MESSAGES = int(sys.argv[1])
 number_of_lines = 0
 
 class Receiver(threading.Thread):
-    def __init__(self, threadID, name, counter):
+    def __init__(self, threadID, name, counter, t):
         threading.Thread.__init__(self)
         self.threadID = threadID
         self.name = name
         self.counter = counter
+        self.t = t
 
     def run(self):
-        self.receive_zmq_message()
+        if self.t == "zmq":
+            self.receive_zmq_messages()
+        else:
+            self.receive_tcp_messages()
 
-    def receive_zmq_message(self):
+    def receive_zmq_messages(self):
         global number_of_lines
         res = 0
         ctx = zmq.Context()
         sock = ctx.socket(zmq.PULL)
         sock.connect("tcp://0.0.0.0:5556")
-        for i in xrange(1, NUMBER_OF_MESSAGES):
+        for i in xrange(1, NUMBER_OF_MESSAGES+1):
             sock.recv()
             number_of_lines += 1
         ctx.destroy()
+
+    def receive_tcp_messages(self):
+        global number_of_lines
+        number_of_lines = 0
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.bind(("127.0.0.1", 6644))
+        s.listen(1)
+
+        conn, addr = s.accept()
+
+        for i in xrange(1, NUMBER_OF_MESSAGES+1):
+            data = conn.recv(18)
+            number_of_lines += 1
+        conn.close()
 
 if not os.environ.has_key('SYSLOG_NG'):
     print("Please set syslog-ng location else test will not work!")
@@ -86,16 +105,16 @@ if len(sys.argv) < 2:
 def main():
     for i in xrange(1,4):
         print("%d. test" % i)
-        measure_source("zmq")
-        measure_source("tcp")
-        print("###################################\n")
-
-    for i in xrange(1,4):
-        print("%d. test" % i)
+        print("###################################")
         measure_destination("zmq")
         measure_destination("tcp")
-        print("###################################\n")
-
+        print("###################################")
+    for i in xrange(1,4):
+        print("%d. test" % i)
+        print("###################################")
+        measure_source("zmq")
+        measure_source("tcp")
+        print("###################################")
 
 def measure_source(source_type):
     remove_if_exists(path + '/test_result')
@@ -144,31 +163,49 @@ def send_tcp_messages():
     s.close()
 
 def arrived_line():
-    with open(path + "/test_result", "r") as res:
-        return len(res.readlines())
+    if os.path.isfile(path + "/test_result"):
+        with open(path + "/test_result", "r") as res:
+            return len(res.readlines())
+    return 0
 
 def measure_destination(destination_type):
-    tr = Receiver(1, "Just an ID", 1)
-    tr.start()
+    global number_of_lines
+    number_of_lines = 0
+
+    server = None
+
+    if destination_type == "zmq":
+        tr = Receiver(1, "Just an ID", 1, "zmq")
+        tr.start()
+    else:
+        tr = Receiver(1, "Just an ID", 1, "tcp")
+        tr.daemon = True
+        tr.start()
 
     remove_if_exists(path + '/slng-test.conf')
     remove_if_exists(path + '/source_file')
     remove_if_exists(path + '/test-persist')
 
     with open(path + '/slng-test.conf', "w") as config_file:
-        config_file.write(conf_destination.replace('SOURCE', 'file("%s");' % (path + '/source_file')).replace('DESTINATION', 'zmq();'))
+        if destination_type == "zmq":
+            config_file.write(conf_destination.replace('SOURCE', 'file("%s");' % (path + '/source_file')).replace('DESTINATION', 'zmq();'))
+        else:
+            config_file.write(conf_destination.replace('SOURCE', 'file("%s");' % (path + '/source_file')).replace('DESTINATION', 'tcp("localhost" port(\"6644\"));'))
     
     with open(path + '/source_file', 'w') as source_file:
-        for i in xrange(0, NUMBER_OF_MESSAGES):
+        for i in xrange(0, NUMBER_OF_MESSAGES+1):
             source_file.write("Test message!!!\n")
 
-    p = subprocess.Popen([os.environ['SYSLOG_NG'], '-Fdve', '-f', path + '/slng-test.conf', '-R', path + '/test-persist'], close_fds=True)
+    start_time = timeit.default_timer()
+    p = subprocess.Popen([os.environ['SYSLOG_NG'], '-F', '-f', path + '/slng-test.conf', '-R', path + '/test-persist'], close_fds=True)
+
     while True:
-        if res == NUMBER_OF_MESSAGES - 1:
-            print("All messages arrived\n")
+        if number_of_lines == NUMBER_OF_MESSAGES:
+            print("Execution time of %s destination is %f" % (destination_type, timeit.default_timer()-start_time))
             break
-        time.sleep(1)
-    p.send_signal(subprocess.signal.SIGINT)
+
+    tr.join()
+    p.kill()
 
 if __name__ == "__main__":
     main()
